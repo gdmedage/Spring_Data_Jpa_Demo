bac7c915d1c855637ec3873604cf64fbf609a535
gdmedage <gdmedage@gmail.com>
2020-04-18 20:38:58 +0530

    New Commit for Master branch

---
+++ src/test/java/com/spring/data/jpa/SpringDataJpaDemo/SpringDataJpaDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.data.jpa.SpringDataJpaDemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDataJpaDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
